5b675b6f36df3abe86874e0234832fdddd03a5ca
Mars <ryan0979798230@gmail.com>
2025-01-07 03:54:26 +0800

    rewrite menu.js
    i learned jQuery (it's peak)

--- menu.js
+++ menu.js
@@ -8,11 +8,14 @@ const navArr = [
   ["changelog.html", "About"]
 ];
 
-let text = "<button class='menu navdrop'><b>&#9776;</b></button>";
+var path = window.location.pathname;
+path = path.split("/")[path.split("/").length - 1];
+
+var text = "<button class='menu navdrop'><b>&#9776;</b></button>";
 text += "<div class='navdropMenu'>";
 for (let i = 0; i < navArr.length; i++){
   text += "<a class='navdropContent";
-  if (window.location.pathname == "/GDTWDemonlist/" + navArr[i][0] || (i == 0 && window.location.pathname == "/GDTWDemonlist/")) {
+  if (path == navArr[i][0] || (i == 0 && path == "")) {
     text += " visiting";
   }
   text += "' target='_self' href='" + navArr[i][0] + "'>" + navArr[i][1] + "</a>";
@@ -21,77 +24,63 @@ text += "</div>";
 text += "<b class='menu'>GDTW Demonlist</b>";
 for (let i = 0; i < navArr.length; i++){
   text += "<a class='menu";
-  if (window.location.pathname == "/GDTWDemonlist/" + navArr[i][0] || (i == 0 && window.location.pathname == "/GDTWDemonlist/")) {
+  if (path == navArr[i][0] || (i == 0 && path == "")) {
     text += " visiting";
   }
   text += "' target='_self' href='" + navArr[i][0] + "'>" + navArr[i][1] + "</a>";
 }
-document.getElementById("menuContainer").innerHTML = text;
+$("#menuContainer").html(text);
 
-//initialize
-let nav = document.getElementsByClassName("menu");
-for (let i = 1; i < nav.length; i++) {
-  nav[i].addEventListener('click', function() {
-    temp = document.getElementsByClassName('visiting');
-    if(temp[0] != nav[i]) {
-      temp[0].classList.add('leave');
-      temp[0].classList.remove('visiting');
-      this.classList.add('visiting');
-    }
-  });
-}
+$("#menu").on("click", function() {
+  if (!$(this).hasClass("visiting")) {
+    $(".visiting").addClass("leave").remove("visiting");
+    $(this).addClass("visiting");
+  }
+});
 
 let dropbtnclicked = false;
 let dropbtndown = false;
 let dropdwnclicked = false;
-let btn = document.getElementsByClassName("navdrop");
-let dropdown = document.getElementsByClassName("navdropMenu");
 
-btn[0].addEventListener('click', function() {
-  if (document.getElementsByClassName("navdropMenu")[0].style.display == "none") {
-    dropbtnclicked = true;
-  }
-  else {
-    document.getElementsByClassName("navdropMenu")[0].style.display = "none";
-    btn[0].blur();
-  }
-});
-
-btn[0].addEventListener("mousedown", function() {
-  if (document.getElementsByClassName("navdropMenu")[0].style.display == "block") {
-    dropbtndown = true;
+$(".navdrop").on({
+  click: function() {
+    if ($(".navdropMenu").css("display") == "none") {
+      dropbtnclicked = true;
+    }
+    else {
+      $(".navdropMenu").css("display", "none").trigger("blur");
+    }
+  },
+  mousedown: function() {
+    if ($(this).css("display") == "block") {
+      dropbtndown = true;
+    }
   }
 });
 
-dropdown[0].addEventListener('mousedown', function() {
-  if (document.getElementsByClassName("navdropMenu")[0].style.display == "block") {
+$(".navdropMenu").on("click", function() {
+  if ($(this).css("display") == "block") {
     dropdwnclicked = true;
   }
 });
 
-document.body.addEventListener('click', function() {
-  if (dropbtnclicked) {
-    document.getElementsByClassName("navdropMenu")[0].style.display = "block";
-    dropbtnclicked = false;
-  }
-});
-
-document.body.addEventListener("mousedown", function() {
-  if (!dropdwnclicked) {
-    if (!dropbtndown) {
-      document.getElementsByClassName("navdropMenu")[0].style.display = "none";
+$("body").on({
+  click: function() {
+    if (dropbtnclicked) {
+      $(".navdropMenu").css("display", "block");
+      dropbtnclicked = false;
     }
+  },
+  mousedown: function() {
+    if (!(dropdwnclicked || dropbtndown)) {
+      $(".navdropMenu").css("display", "none");
+    }
+    dropbtnclicked = false;
+    dropbtndown = false;
   }
-  dropdwnclicked = false;
-  dropbtndown = false;
 });
 
-
-let nav2 = document.getElementsByClassName("navdropContent");
-for (let i = 1; i < nav2.length; i++) {
-  nav2[i].addEventListener('click', function() {
-    temp2 = document.getElementsByClassName('visiting');
-    temp2[0].classList.remove('visiting');
-    this.classList.add('visiting');
-  });
-}
+$(".navdropContent").on("click", function() {
+  $(".visiting").removeClass("visiting");
+  $(this).addClass("visiting");
+});
