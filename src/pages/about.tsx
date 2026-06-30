9d83e88111b60b65e43c434bc6a2e0ec37e12758
Mars <ryan0979798230@gmail.com>
2026-06-30 14:51:44 +0800

    test modify

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,7 @@ export function About() {
     return (
         <div>
             <h1>Welcome to My Site</h1>
-            <p>This is the home page rewritten in React!</p>
+            <p>This is the about page rewritten in React!</p>
         </div>
     );
 }
