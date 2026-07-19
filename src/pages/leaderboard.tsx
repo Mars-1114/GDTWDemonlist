2e2b4438d91f864de3097c28b356c5deac94e0c9
Mars <ryan0979798230@gmail.com>
2026-07-20 02:11:02 +0800

    record submission

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -4,9 +4,9 @@ import {LeaderboardList} from "../components/leaderboard_list.tsx";
 import {LeaderboardDetail} from "../components/leaderboard_detail.tsx";
 
 export function LeaderboardPage({data, list_type}: {data: obj.Data, list_type: "classic" | "platformer"}) {
-    const [player, setPlayer] = useState<string>(data.leaderboard.classic.keys().next().value!);
     const demonlist = list_type === "classic" ? data.demonlist.classic : data.demonlist.platformer;
     const leaderboard = list_type === "classic" ? data.leaderboard.classic : data.leaderboard.platformer;
+    const [player, setPlayer] = useState<string>(leaderboard.keys().next().value!);
 
     return (
         <div style={{display: "flex", flexDirection: "row", height: "500px"}}>
